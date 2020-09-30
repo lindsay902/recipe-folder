d674be32b309fcfeb3a39cb2d5330e18814448e7
Lindsay902 <lindsaymlee88@gmail.com>
2020-09-30 09:37:15 -0500

    Initial Commit

---
+++ jquery.js
@@ -0,0 +1,83 @@
+$(document).ready(() => {
+
+    $('.paper').hide();
+
+    $("#tabTwo").hide();
+    $("#tabThree").hide();
+    $("#tabFour").hide();
+    $("#tabFive").hide();
+
+
+    $('.tabOne').on('click', function() {
+    $('a.switch').removeClass("activeTab");
+    $(this).addClass("activeTab");
+      $("#tabOne").show();
+      $("#tabTwo").hide();
+      $("#tabThree").hide();
+      $("#tabFour").hide();
+      $("#tabFive").hide();
+      event.preventDefault();
+	});
+  
+    $('.tabTwo').on('click', function() {
+        $('a.switch').removeClass("activeTab");
+        $(this).addClass("activeTab");
+        $("#tabOne").hide();
+        $("#tabTwo").show()
+        $("#tabThree").hide();
+        $("#tabFour").hide();
+        $("#tabFive").hide();
+        event.preventDefault();
+        });
+    
+    $('.tabThree').on('click', function() {
+        $('a.switch').removeClass("activeTab");
+        $(this).addClass("activeTab");
+        $("#tabOne").hide();
+        $("#tabTwo").hide();
+        $("#tabThree").show();
+        $("#tabFour").hide();
+        $("#tabFive").hide();
+        event.preventDefault();
+        });
+    
+    $('.tabFour').on('click', function() {
+        $('a.switch').removeClass("activeTab");
+        $(this).addClass("activeTab");
+        $("#tabOne").hide();
+        $("#tabTwo").hide();
+        $("#tabThree").hide();
+        $("#tabFour").show();
+        $("#tabFive").hide();
+        event.preventDefault();
+        });
+
+    $('.tabFive').on('click', function() {
+        $('a.switch').removeClass("activeTab");
+        $(this).addClass("activeTab");
+        $("#tabOne").hide();
+        $("#tabTwo").hide();
+        $("#tabThree").hide();
+        $("#tabFour").hide();
+        $("#tabFive").show();
+        event.preventDefault();
+        });
+    
+    $('.add-content').on('click', () => {
+        $('.paper').show();
+    })
+
+    $('.close-button').on('click', () => {
+        $('.paper').hide();
+    })
+    
+    let recipeName = $('.recipe-name').val();
+
+    //select the active class?
+    $('.save-button').on('click', () => {
+        $('<li>'+recipeName+'</li>').appendTo('#recipe-name');        
+    })
+
+    
+
+});
